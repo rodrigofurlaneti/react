a294426affc1dc5d3939459190d0e63e7bef7e73
Rodrigo Furlaneti <rodrigofurlaneti31@hotmail.com>
2019-09-07 08:35:08 -0300

    Rodrigo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React, {Component} from 'react';
-//import logo from './logo.svg';
-//import './App.css';
+import React, { Component } from 'react';
+import Routes from './routes';
+
 import Header from "./components/Header";
 import Main from './pages/main/index';
 const App = () => (
     <div className="App">
-      <Header />
-      <Main />
+        <Header />
+        <Routes />
     </div>
 );
 export default App;
--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,7 +1,8 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import api from '../../services/api';
+import { Link } from 'react-router-dom';
 import './styles.css';
-export default class Main extends Component{
+export default class Main extends Component {
     state = {
         usuarios: [],
         usuarioInfo: {},
@@ -13,21 +14,21 @@ export default class Main extends Component{
     loadUsuarios = async (page = 1) => {
         const response = await api.get(`/usuarios?page=${page}`);
         const { docs, ...usuarioInfo } = response.data;
-        this.setState({ usuarios: docs , usuarioInfo, page});
+        this.setState({ usuarios: docs, usuarioInfo, page });
     };
     prevPage = () => {
-        const {page, usuarioInfo} = this.state;
-        if(page === 1) return;
+        const { page, usuarioInfo } = this.state;
+        if (page === 1) return;
         const pageNumber = page - 1;
         this.loadUsuarios(pageNumber);
     };
     nextPage = () => {
-        const {page, usuarioInfo} = this.state;
-        if(page === usuarioInfo.pages) return;
+        const { page, usuarioInfo } = this.state;
+        if (page === usuarioInfo.pages) return;
         const pageNumber = page + 1;
         this.loadUsuarios(pageNumber);
     };
-    render(){
+    render() {
         const { usuarios } = this.state;
         return (
             <div className="usuario-list">
@@ -36,12 +37,13 @@ export default class Main extends Component{
                         <label>E-mail:</label><strong>{usuarios.email}</strong>
                         <p><label>Senha:</label>{usuarios.senha}</p>
                         <p><label>Cpf:</label>{usuarios.cpf}</p>
+                        <label>Profile:</label><Link to={`/usuarios/${usuarios._id}`}>Acessar</Link>
                     </article>
                 ))}
                 <div className="actions">
                     <button onClick={this.prevPage}>Anterior</button>
                     <button onClick={this.nextPage}>Próximo</button>
-                </div>            
+                </div>
             </div>
         )
     }
